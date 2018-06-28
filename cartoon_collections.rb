2eb1e4a2a3501651fe68962b2d08ad0c10b3d1bd
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-28 03:57:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,9 @@ def long_planeteer_calls(array)
   # code an argument here
   # Your code here
   
-  array.any?{ |x| x.length > 4}
+  array.any?{|x| x.length > 4}
 end
   
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
