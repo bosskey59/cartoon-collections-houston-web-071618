b6b2f615b242c8445878f4a6f21933c0997b01fd
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-28 04:15:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each_with_index do |val, index|
+  array.find do |x|
     
     
   end
