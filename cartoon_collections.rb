0365537bb9bc96f3f5e0305e1e937697074f308c
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-28 03:54:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  # code an argument here
   # Your code here
+  array.each_with_index do |val, index|
+    puts"#{index+1} #{val}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
+  # code an argument here
   # Your code here
+  array.collect{|x| x.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  # code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
